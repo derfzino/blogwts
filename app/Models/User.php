1198667efdf8b7981f3206b20b77b80818966e80
derfzino <sandery37@gmail.com>
2026-05-18 15:39:58 +0300

    feat: implement API authentication endpoints using Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
-use Orchid\Filters\Types\Like;
-use Orchid\Filters\Types\Where;
-use Orchid\Filters\Types\WhereDateStartEnd;
-use Orchid\Platform\Models\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Orchid\Platform\Models\User as BaseUser; 
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends BaseUser
 {
+    use HasApiTokens, Notifiable;
+
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array
      */
     protected $fillable = [
         'name',
@@ -21,49 +21,19 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
+     * The attributes that should be hidden for serialization.
      */
     protected $hidden = [
         'password',
         'remember_token',
-        'permissions',
     ];
 
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
+     * The attributes that should be cast.
      */
     protected $casts = [
-        'permissions'          => 'array',
-        'email_verified_at'    => 'datetime',
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
-
-    /**
-     * The attributes for which you can use filters in url.
-     *
-     * @var array
-     */
-    protected $allowedFilters = [
-           'id'         => Where::class,
-           'name'       => Like::class,
-           'email'      => Like::class,
-           'updated_at' => WhereDateStartEnd::class,
-           'created_at' => WhereDateStartEnd::class,
-    ];
-
-    /**
-     * The attributes for which can use sort in url.
-     *
-     * @var array
-     */
-    protected $allowedSorts = [
-        'id',
-        'name',
-        'email',
-        'updated_at',
-        'created_at',
-    ];
+    
 }
