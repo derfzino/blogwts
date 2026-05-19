04ce6a40b889f528bfb00ad8af62c31b8cdf8285
derfzino <sandery37@gmail.com>
2026-05-19 12:50:22 +0300

    fix: API structure Requests

---
+++ app/Http/Requests/Post/UpdatePostRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $post = $this->route('post');
+        return $post && $this->user()->id === $post->user_id;
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255|min:3',
+            'body' => 'sometimes|required|string|min:10',
+            'is_published' => 'sometimes|boolean',
+        ];
+    }
+}
